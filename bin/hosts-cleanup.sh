0a2bc9ed8432b676265f9978013973e7432ac686
Boštjan Škufca <bostjan@a2o.si>
2011-08-01 18:44:44 +0200

    Fixed cleanup script

--- bin/hosts-cleanup.sh
+++ bin/hosts-cleanup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 PREFIX="$1"
+rm  -f ${PREFIX}var/awstats/data/*
+rm -rf ${PREFIX}var/collectd/*
+rm -rf ${PREFIX}var/symon/rrds/*
+rm -rf ${PREFIX}var/ganglia/rrds/*
+rm -rf ${PREFIX}var/mail/virtual/*
+
+# Remove these stale entries at some point FIXME
 rm  -f ${PREFIX}fs/var/awstats/data/*
 rm -rf ${PREFIX}fs/var/collectd/*
 rm -rf ${PREFIX}fs/var/symon/rrds/*
