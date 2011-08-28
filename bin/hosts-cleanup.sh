c64dd1b4151ad0abcdc2bde30b034c76cfcfec75
Bostjan Skufca <bostjan@a2o.si>
2011-08-28 23:25:52 +0200

    Fixed cleanup script, because var was moved to separate rsync module

--- bin/hosts-cleanup.sh
+++ bin/hosts-cleanup.sh
@@ -7,12 +7,5 @@ rm -rf ${PREFIX}var/symon/rrds/*
 rm -rf ${PREFIX}var/ganglia/rrds/*
 rm -rf ${PREFIX}var/mail/virtual/*
 
-# Remove these stale entries at some point FIXME
-rm  -f ${PREFIX}fs/var/awstats/data/*
-rm -rf ${PREFIX}fs/var/collectd/*
-rm -rf ${PREFIX}fs/var/symon/rrds/*
-rm -rf ${PREFIX}fs/var/ganglia/rrds/*
-rm -rf ${PREFIX}fs/var/mail/virtual/*
-
 ### Usage:
 # $0 '/var/backup/*/2011-06-*/'
